4e7be0e33db4aa0b2ef7e1ecbbf0691248d40bd5
Shikhar164 <shikharbhardwaj164@gmail.com>
2023-06-01 02:45:36 +0530

    Version 1

---
+++ router-project-starter/src/pages/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div className='flex justify-center items-center text-white text-3xl h-full'>
+            Welcome To StudyNotation
+        </div>
+    );
+}
+
+export default Dashboard;
